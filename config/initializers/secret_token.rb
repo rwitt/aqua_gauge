f2c16357fa5b64e1fee393ffe2581ca59fb13f92
Rob Wittenberg <rob.wittenberg@gmail.com>
2013-09-08 14:32:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AquaGauge::Application.config.secret_key_base = 'e87f032cbda86efebd9355ee06da19f2c2a4321602a4b185e5b6c63817d5936b15e8af6eb9b1ea9d870396ebb10676dcf76214a17656c20959657d4c0b612edd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AquaGauge::Application.config.session_store :cookie_store, key: '_aqua_gauge_session'
